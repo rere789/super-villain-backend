38778205055524a0def21789f4237c3ab0ec635d
github username <github email address>
2019-08-20 18:38:16 -0400

    log in

--- super-villain-backend/config/routes.rb
+++ super-villain-backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      post '/login', to: 'users#login'
       resources :users
       resources :results
       resources :cards
